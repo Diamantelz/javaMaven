f25d5588eef1492eb4c9a812894a34905e63b29d
Dmitriy Miheenko <dmitriy.mikheenko@mail.ru>
2023-05-26 19:53:00 +0300

    main file has been updated

--- src/main/java/lesson16/Main.java
+++ src/main/java/lesson16/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Person person = PersonAPI.getPersonFromAPI();
         System.out.println(person);
-        System.out.println("git test");
     }
 }
