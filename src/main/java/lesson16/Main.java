b2cef52886287860d8c348aa3b13128c13210bf5
Dmitriy Miheenko <dmitriy.mikheenko@mail.ru>
2023-05-26 19:50:21 +0300

    git branch conflict is fixed

--- src/main/java/lesson16/Main.java
+++ src/main/java/lesson16/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Person person = PersonAPI.getPersonFromAPI();
         System.out.println(person);
+        System.out.println("git test");
     }
 }
