621a9e5a274d304df579213b325ab6d1eb6f9307
Dmitriy Miheenko <dmitriy.mikheenko@mail.ru>
2023-06-09 17:11:17 +0300

    more examples with lambda are added

--- src/main/java/lesson19/Main.java
+++ src/main/java/lesson19/Main.java
@@ -4,6 +4,7 @@ import lesson16.Person;
 import lesson16.PersonAPI;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class Main {
             System.out.println(person);
         }
         System.out.println("-------");
-        LambdaExample.sortLambda(persons, new Predicate<Person>() {
+        LambdaExample.sortLambda(persons, new Predicate<>() {
             @Override
             public boolean test(Person person) {
                 return person.getAge() < 30;
@@ -25,6 +26,16 @@ public class Main {
 
         LambdaExample.sortLambda(persons, p -> p.getAge() < 30);
         System.out.println("-------");
+
+        LambdaExample.sortLambda(persons, p -> p.getCountry().equals("Spain"), c -> {
+            c.setCountry("QWE");
+            System.out.println(c);});
+        System.out.println("-------");
+
+        LambdaExample.sortLambda(persons, p -> p.getCountry().equals("Spain"), c -> {
+            c.setCountry("QWE");
+            System.out.println(c);}, p -> p.length());
+
     }
 }
 
@@ -32,11 +43,31 @@ interface Predicate<T> {
     boolean test(T t);
 }
 
+interface IConsumer<T> {
+    void accept(T t);
+}
+
 class LambdaExample {
     public static void sortLambda(List<Person> list, Predicate<Person> predicate) {
         for (Person person : list) {
             if (predicate.test(person)) {
                 System.out.println(person);
+            }
+        }
+    }
+    public static void sortLambda(List<Person> list, Predicate<Person> predicate, IConsumer<Person> consumer) {
+        for (Person person : list) {
+            if (predicate.test(person)) {
+                consumer.accept(person);
+            }
+        }
+    }
+    public static void sortLambda(List<Person> list, Predicate<Person> predicate, IConsumer<Person> consumer,
+                                  Function<String, Integer> function) {
+        for (Person person : list) {
+            if (predicate.test(person)) {
+                consumer.accept(person);
+                function.apply(person.getCountry());
             }
         }
     }
